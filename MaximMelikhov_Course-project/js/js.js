817ae22e6de4145fc0725446515c0e01a6593b9c
Maskati <melikhov-maksim@rambler.ru>
2021-11-22 15:19:20 +0300

    Издания без 320

--- MaximMelikhov_Course-project/js/js.js
+++ MaximMelikhov_Course-project/js/js.js
@@ -166,7 +166,7 @@ $( function() {
 // events
 
 let btn = document.querySelector(".events_btn");
-const slider = document.querySelector('.events_block');
+const slider = document.querySelector('.newSwiper');
 let allitems = document.querySelectorAll(".events_item");
 btn.addEventListener("click", function() {
   allitems.forEach(item => {
@@ -183,7 +183,7 @@ let mySwiper;
 function mobileSlider() {
 	if (window.innerWidth <= 320 && slider.dataset.mobile == 'false') {
 		mySwiper = new Swiper(slider, {
-			slidesPerView: 0,
+			slidesPerView: 1,
 			spaceBetween: 10,
 			loop: true,
 			slideClass: 'events_item',
@@ -211,3 +211,132 @@ window.addEventListener('resize', () => {
 });
 
 
+// editions
+
+const slidereditions = document.querySelector('.newSwiperf');
+let allitemseditions = document.querySelectorAll(".editions_swiper-item");
+
+
+
+let mySwipereditors;
+
+function initSlide() {
+		mySwipereditions = new Swiper(slidereditions, {
+			slidesPerView: 3,
+			spaceBetween: 50,
+      slidesPerGroup: 2,
+			loop: true,
+			slideClass: 'editions_swiper-item',
+      pagination: {
+        el: ".swiper-pagination--right",
+        type: "fraction"
+      },
+      navigation: {
+        nextEl: ".swiper-btn--next",
+        prevEl: ".swiper-btn--prev"
+      },
+      breakpoints: {
+        1400: {
+          slidesPerView: 2,
+          grid: {
+            rows: 1
+          },
+          spaceBetween: 50
+        },
+        1024: {
+          slidesPerView: 2,
+          grid: {
+            rows: 1
+          },
+          spaceBetween: 50
+        },
+        768: {
+          slidesPerView: 2,
+          grid: {
+            rows: 1
+          },
+          spaceBetween: 50
+        }
+      },
+		});
+}
+
+
+initSlide() 
+mySwipereditions.on('resize', function (e) {
+  console.log();
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    this.destroy();
+    mySwiper = underfined;
+  } 
+  
+});
+
+
+
+// let button = ".h2-click";
+// let labels = ".checkbox--label";
+// let labelsList = ".checkbox-list";
+// let labelsListActive = "checklist-active";
+// let labelActive = "checkbox--label-active";
+// let animationClass = "animation-test";
+// let inputCheckbox = ".checkbox";
+
+// function checkboxToggle(a, b, c, labelsListActive, labelActive, animationClass, inputCheckbox) {
+//   let btn = document.querySelector(a);
+//   let labels = document.querySelectorAll(b);
+//   let listLabels = document.querySelector(c);
+// btn.addEventListener("click", toggleSpoiler);
+//   btn.addEventListener("keyup", function(e) {
+//     console.log(e.key);
+//     if (e.code === "Enter") {
+//       toggleSpoiler();
+//     }
+//   })    
+// function toggleSpoiler() {
+//     if (!listLabels.classList.contains(labelsListActive)) {
+//     listLabels.classList.add(labelsListActive);
+//     labels.forEach(item => {
+//      // item.classList.add("checkbox--label-active");
+//       animationItem(item, labelActive, animationClass, "add");
+//     })
+//   } else {
+//     listLabels.classList.remove(labelsListActive);
+//     labels.forEach(item => {
+//     	if (item.querySelector(inputCheckbox).checked) {
+//     	animationItem(item, labelActive, animationClass, "add");
+//       } else {
+//         animationItem(item, labelActive, animationClass, "remove");
+//       }
+//       });
+//   }
+//   labels.forEach(item => {
+//     item.addEventListener("click", function() {
+//       if (!listLabels.classList.contains(labelsListActive)) {
+//         animationItem(this, labelActive, animationClass, "remove");
+//       }
+//     });
+//   })
+// }
+// function animationItem(item, class1, class2, f) {
+//  if (f === "add") {
+//     item.classList.add(class1);
+//   setTimeout(function() {
+//     item.classList.add(class2)
+//   }, 100);
+
+//  } else {
+//      item.classList.remove(class2);
+//   setTimeout(function() {
+//     item.classList.remove(class1)
+//   }, 300);
+//   }
+ 
+// }
+
+
+// }
+
+
+
+// checkboxToggle(button, labels, labelsList, labelsListActive, labelActive, animationClass, inputCheckbox);
